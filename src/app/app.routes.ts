ab1e20c82cef717eb6f54ea36647afbb206e23ce
josipgavranic <40172887+josipgavranic@users.noreply.github.com>
2023-11-21 19:15:48 +0100

    sdf

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home.component';
-import { ProfileComponent } from './components/profile.component';
-import { RestService } from './services/rest.service';
-
-export const routes = [
-    {path: 'home', component: HomeComponent},
-    {path: 'profile', component: ProfileComponent},
-];
